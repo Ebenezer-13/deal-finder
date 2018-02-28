70d061bf022e22ac5f0ca0d71ed9239cffc68719
Sam Hopkins <shopkins@uk.ibm.com>
2018-02-28 20:23:09 +0000

    Create Header component as component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,32 @@
 import * as React from "react";
-import { Route, RouteComponentProps } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import FlexContainer from "./FlexContainer";
+// import FlexContainer from "./FlexContainer";
 
 const Bar = styled.header`
   padding-left: 10px;
-  background-color: ${props => props.theme.primary};
-  color: ${props => props.theme.textOnPrimary};
+  background-color: navy;
+  color: white;
   display: flex;
   align-items: center;
   min-height: 50px;
 `;
 
-const titleComponent = (title, backButtonUrl) => ({ history }) => {
-  return (
-    <Container>
-      {(backButtonUrl && (
-        <BackImage
-          src="/arrow-back.svg"
-          onClick={() => history.push(backButtonUrl)}
-        />
-      )) || <MenuImage src="menu.svg" />}
-      {title}
-    </Container>
-  );
-};
+// const Tab = styled(Link)`color: white;`;
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <Bar>
+        <Link to="/">Home</Link>
+        <Link to="/funding-reqs">Funding Requirements</Link>
+        <Link to="/project-details">Project Details</Link>
+        <Link to="/initial-options">Initial Funding Options</Link>
+        <Link to="/about-you">About You</Link>
+        <Link to="/document-submission">Document Submission</Link>
+        <Link to="/thank-you">Thank You</Link>
+        <p>{this.props.lol}</p>
+      </Bar>
+    );
+  }
+}
