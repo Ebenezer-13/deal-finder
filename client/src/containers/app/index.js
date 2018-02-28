30249b27faa9e1a37ee8f7935e0a9cf6449908fc
Sam Hopkins <shopkins@uk.ibm.com>
2018-02-28 21:32:58 +0000

    Styled home page

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Header from "../../components/Header";
-import Home from "../home";
+import Home from "../Home";
 import FundingReqs from "../FundingReqs";
 import ProjectDetails from "../ProjectDetails";
 import InitialOptions from "../InitialOptions";
