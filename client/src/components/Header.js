3e7af9581fe4f3b4b53982f290b8ee720d4775b4
Sam Hopkins <shopkins@uk.ibm.com>
2018-02-28 21:31:41 +0000

    Add some style to header links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,27 +4,31 @@ import styled from "styled-components";
 // import FlexContainer from "./FlexContainer";
 
 const Bar = styled.header`
-  padding-left: 10px;
+  width: 100%;
   background-color: navy;
   color: white;
   display: flex;
+  flex-direction: row;
+  justify-content: space-around;
   align-items: center;
   min-height: 50px;
 `;
+
+const StylishLink = styled(Link)`color: white !important;`;
 
 // const Tab = styled(Link)`color: white;`;
 
 export default function Header({ lol }) {
   return (
     <Bar>
-      <Link to="/">Home</Link>
-      <Link to="/funding-reqs">Funding Requirements</Link>
-      <Link to="/project-details">Project Details</Link>
-      <Link to="/initial-options">Initial Funding Options</Link>
-      <Link to="/about-you">About You</Link>
-      <Link to="/document-submission">Document Submission</Link>
-      <Link to="/thank-you">Thank You</Link>
-      {lol}
+      <StylishLink to="/">Home</StylishLink>
+      <StylishLink to="/funding-reqs">Funding Requirements</StylishLink>
+      <StylishLink to="/project-details">Project Details</StylishLink>
+      <StylishLink to="/initial-options">Initial Funding Options</StylishLink>
+      <StylishLink to="/about-you">About You</StylishLink>
+      <StylishLink to="/document-submission">Document Submission</StylishLink>
+      <StylishLink to="/thank-you">Thank You</StylishLink>
+      {/* {lol} */}
     </Bar>
   );
 }
